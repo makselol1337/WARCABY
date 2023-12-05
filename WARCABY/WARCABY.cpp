4d7abd0afe82a12214e7d9e8204877565d142969
makselol1337 <maks.palkanski1@gmail.com>
2023-12-05 16:25:28 +0100

    V0.05

--- WARCABY/WARCABY.cpp
+++ WARCABY/WARCABY.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int main() {
-	int tablica[8][8] = {
+	char tablica[8][8] = {
 		{'-', 'C', '-', 'C', '-', 'C', '-', 'C'},
 		{'C', '-', 'C', '-', 'C', '-', 'C', '-'},
 		{'-', 'C', '-', 'C', '-', 'C', '-', 'C'},
@@ -13,10 +13,14 @@ int main() {
 		{'-', 'B', '-', 'B', '-', 'B', '-', 'B'},
 		{'B', '-', 'B', '-', 'B', '-', 'B', '-'}
 	};
+	
+	for (int rzad = 0; rzad < 8; rzad++) {
+		for (int kolumna = 0; kolumna < 8; kolumna++) {
+			cout << tablica[rzad][kolumna] << " ";
+		}
+		cout << endl;
 
-
-
-
+	}
 
 
 

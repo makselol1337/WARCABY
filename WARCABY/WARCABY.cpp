a8b2afa3cde621dad8267e9495f0dcc1efabe93c
makselol1337 <maks.palkanski1@gmail.com>
2023-12-05 15:49:52 +0100

    V0.01

--- WARCABY/WARCABY.cpp
+++ WARCABY/WARCABY.cpp
@@ -1,2 +1,25 @@
 ﻿#include <iostream>
 using namespace std;
+
+
+int main() {
+	int tablica[8][8] = {
+		{'C', '-', 'C', '-', 'C', '-', 'C', '-'},
+		{'-', 'C', '-', 'C', '-', 'C', '-', 'C'},
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
